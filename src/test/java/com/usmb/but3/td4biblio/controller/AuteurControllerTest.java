656d3596ca6bc363f93883d4490541afe33fee78
Akieldamas <122918848+Akieldamas@users.noreply.github.com>
2026-06-10 10:33:28 +0200

    migration technique

--- src/test/java/com/usmb/but3/td4biblio/controller/AuteurControllerTest.java
+++ src/test/java/com/usmb/but3/td4biblio/controller/AuteurControllerTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.resttestclient.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import com.usmb.but3.td4biblio.entity.Auteur;
 import static org.assertj.core.api.Assertions.assertThat;
--- src/test/java/com/usmb/but3/td4biblio/controller/LivreControllerMockTest.java
+++ src/test/java/com/usmb/but3/td4biblio/controller/LivreControllerMockTest.java
@@ -3,7 +3,7 @@ package com.usmb.but3.td4biblio.controller;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,7 +17,7 @@ import com.usmb.but3.td4biblio.entity.Livre;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfiguration
 public class LivreControllerMockTest {
 
 	@Test
