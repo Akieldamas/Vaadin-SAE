2d6997e793776e245eb66522eaeff0c5656e495a
EnzoBenechIUT <enzo.benech@etu.univ-smb.fr>
2026-06-11 15:23:42 +0200

    fix | retrait de code inutile

--- src/main/java/com/usmb/but3/td4biblio/repository/EmpruntRepo.java
+++ src/main/java/com/usmb/but3/td4biblio/repository/EmpruntRepo.java
@@ -2,11 +2,9 @@ package com.usmb.but3.td4biblio.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import com.usmb.but3.td4biblio.entity.Emprunt;
 import com.usmb.but3.td4biblio.entity.EmpruntId;
 
-@Repository
 public interface EmpruntRepo extends JpaRepository<Emprunt, EmpruntId> {
     
     // Retrouver les emprunts d'un utilisateur via son ID
