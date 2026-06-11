214907ad3c2edbc33defa5705eb91295f6d98176
GONTARDLukas <lukas.gontard@etu.univ-smb.fr>
2026-06-11 13:49:59 +0200

    feat #8 add regex validator for wikipedia link

--- src/main/java/com/usmb/but3/td4biblio/view/AuteurView.java
+++ src/main/java/com/usmb/but3/td4biblio/view/AuteurView.java
@@ -53,7 +53,7 @@ public class AuteurView extends VerticalLayout {
 		grid.setHeight("300px");
 		grid.setColumns("id", "nom", "prenom", "nationalite", "dateNaissance", "dateDeces", "villeNaissance","lienWikipedia");
 		grid.getColumnByKey("id").setWidth("50px").setFlexGrow(0);
-
+		
 		filter.setPlaceholder("Filtrer par nom");
 
 		// Hook logic to components
