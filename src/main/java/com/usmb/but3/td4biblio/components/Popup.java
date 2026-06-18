44c55fd3765ac3d6696a2dedcb51083a60ba7daf
Alexandre OSTIER <alexandre.ostier@etu.univ-smb.fr>
2026-06-18 11:20:23 +0200

    fix #- semaines -> jours pour le popup

--- src/main/java/com/usmb/but3/td4biblio/components/Popup.java
+++ src/main/java/com/usmb/but3/td4biblio/components/Popup.java
@@ -12,7 +12,7 @@ public class Popup extends Composite<VerticalLayout> {
     public Popup(int temps){
         Dialog dialog = new Dialog();
         H1 title = new H1("Expiration abonnement !");
-        Paragraph descParagraph = new Paragraph("Attention !! Il vous reste "+Integer.toString(temps)+" semaines avant l'expiration de votre compte...");
+        Paragraph descParagraph = new Paragraph("Attention !! Il vous reste "+Integer.toString(temps)+" jours avant l'expiration de votre compte...");
         descParagraph.getStyle().set("text-wrap", "wrap");
         dialog.add(title,descParagraph);
         VerticalLayout layout = getContent();
