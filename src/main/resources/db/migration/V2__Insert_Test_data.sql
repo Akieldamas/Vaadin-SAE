0002df3af4ee4e4f9017c7881a364c43d8151b46
EnzoBenechIUT <enzo.benech@etu.univ-smb.fr>
2026-06-18 11:00:34 +0200

    mise à jour des auteurs

--- src/main/resources/db/migration/V2__Insert_Test_data.sql
+++ src/main/resources/db/migration/V2__Insert_Test_data.sql
@@ -90,18 +90,55 @@ INSERT INTO editeur (id, nom, adresse, lien_site_web, lien_wikipedia) VALUES
 --  AUTEURS
 -- ------------------------------------------------------------
 INSERT INTO auteur (id, nom, prenom, nationalite, date_naissance, date_deces, ville_naissance, lien_wikipedia) VALUES
-    (1,  'Orwell',       'George',       'Britannique',  '1903-06-25', '1950-01-21', 'Motihari',        'https://fr.wikipedia.org/wiki/George_Orwell'),
-    (2,  'Camus',        'Albert',       'Française',    '1913-11-07', '1960-01-04', 'Dréan',           'https://fr.wikipedia.org/wiki/Albert_Camus'),
-    (3,  'Le-Guin',      'Ursula-K.',    'Américaine',   '1929-10-21', '2018-01-22', 'Berkeley',        'https://fr.wikipedia.org/wiki/Ursula_K._Le_Guin'),
-    (4,  'King',         'Stephen',      'Américaine',   '1947-09-21', NULL,         'Portland',        'https://fr.wikipedia.org/wiki/Stephen_King'),
-    (5,  'Nolan',        'Christopher', 'Britannique',  '1970-07-30', NULL,         'Londres',         'https://fr.wikipedia.org/wiki/Christopher_Nolan'),
-    (6,  'Spielberg',    'Steven',       'Américaine',   '1946-12-18', NULL,         'Cincinnati',      'https://fr.wikipedia.org/wiki/Steven_Spielberg'),
-    (7,  'Zimmer',       'Hans',         'Allemande',    '1957-09-12', NULL,         'Francfort',       'https://fr.wikipedia.org/wiki/Hans_Zimmer'),
-    (8,  'Cohen',        'Leonard',      'Canadienne',   '1934-09-21', '2016-11-07', 'Montréal',        'https://fr.wikipedia.org/wiki/Leonard_Cohen'),
-    (9,  'Miyazaki',     'Hayao',        'Japonaise',    '1941-01-05', NULL,         'Tokyo',           'https://fr.wikipedia.org/wiki/Hayao_Miyazaki'),
-    (10, 'Duras',        'Marguerite',   'Française',    '1914-04-04', '1996-03-03', 'Gia Dinh',        'https://fr.wikipedia.org/wiki/Marguerite_Duras'),
-    (11, 'Tolkien',      'J.R.R.',       'Britannique',  '1892-01-03', '1973-09-02', 'Bloemfontein',    'https://fr.wikipedia.org/wiki/J._R._R._Tolkien'),
-    (12, 'Villeneuve',   'Denis',        'Canadienne',   '1967-10-03', NULL,         'Gentilly',        'https://fr.wikipedia.org/wiki/Denis_Villeneuve');
+    (1,  'Orwell',       'George',       'Britannique',      '1903-06-25', '1950-01-21', 'Motihari',             'https://fr.wikipedia.org/wiki/George_Orwell'),
+    (2,  'Camus',        'Albert',       'Française',        '1913-11-07', '1960-01-04', 'Dréan',                'https://fr.wikipedia.org/wiki/Albert_Camus'),
+    (3,  'Le Guin',      'Ursula K.',    'Américaine',       '1929-10-21', '2018-01-22', 'Berkeley',             'https://fr.wikipedia.org/wiki/Ursula_K._Le_Guin'),
+    (4,  'King',         'Stephen',      'Américaine',       '1947-09-21', NULL,         'Portland',             'https://fr.wikipedia.org/wiki/Stephen_King'),
+    (5,  'Nolan',        'Christopher',  'Britannique',      '1970-07-30', NULL,         'Londres',              'https://fr.wikipedia.org/wiki/Christopher_Nolan'),
+    (6,  'Spielberg',    'Steven',       'Américaine',       '1946-12-18', NULL,         'Cincinnati',           'https://fr.wikipedia.org/wiki/Steven_Spielberg'),
+    (7,  'Zimmer',       'Hans',         'Allemande',        '1957-09-12', NULL,         'Francfort',            'https://fr.wikipedia.org/wiki/Hans_Zimmer'),
+    (8,  'Cohen',        'Leonard',      'Canadienne',       '1934-09-21', '2016-11-07', 'Montréal',             'https://fr.wikipedia.org/wiki/Leonard_Cohen'),
+    (9,  'Miyazaki',     'Hayao',        'Japonaise',        '1941-01-05', NULL,         'Tokyo',                'https://fr.wikipedia.org/wiki/Hayao_Miyazaki'),
+    (10, 'Duras',        'Marguerite',   'Française',        '1914-04-04', '1996-03-03', 'Gia Dinh',             'https://fr.wikipedia.org/wiki/Marguerite_Duras'),
+    (11, 'Tolkien',      'J.R.R.',       'Britannique',      '1892-01-03', '1973-09-02', 'Bloemfontein',         'https://fr.wikipedia.org/wiki/J._R._R._Tolkien'),
+    (12, 'Villeneuve',   'Denis',        'Canadienne',       '1967-10-03', NULL,         'Gentilly',             'https://fr.wikipedia.org/wiki/Denis_Villeneuve'),
+    (13, 'Hugo',         'Victor',       'Française',        '1802-02-26', '1885-05-22', 'Besançon',             'https://fr.wikipedia.org/wiki/Victor_Hugo'),
+    (14, 'Dostoïevski',  'Fiodor',       'Russe',            '1821-11-11', '1881-02-09', 'Moscou',               'https://fr.wikipedia.org/wiki/Dostoïevski'),
+    (15, 'García Márquez','Gabriel',     'Colombienne',      '1927-03-06', '2014-04-17', 'Aracataca',            'https://fr.wikipedia.org/wiki/Gabriel_García_Márquez'),
+    (16, 'Flaubert',     'Gustave',      'Française',        '1821-12-12', '1880-05-08', 'Rouen',                'https://fr.wikipedia.org/wiki/Gustave_Flaubert'),
+    (17, 'Proust',       'Marcel',       'Française',        '1871-07-10', '1922-11-18', 'Auteuil',              'https://fr.wikipedia.org/wiki/Marcel_Proust'),
+    (18, 'Woolf',        'Virginia',     'Britannique',      '1882-01-25', '1941-03-28', 'Londres',              'https://fr.wikipedia.org/wiki/Virginia_Woolf'),
+    (19, 'Borges',       'Jorge Luis',   'Argentine',        '1899-08-24', '1986-06-14', 'Buenos Aires',         'https://fr.wikipedia.org/wiki/Jorge_Luis_Borges'),
+    (20, 'Houellebecq',  'Michel',       'Française',        '1956-02-26', NULL,         'Saint-Pierre',         'https://fr.wikipedia.org/wiki/Michel_Houellebecq'),
+    (21, 'Modiano',      'Patrick',      'Française',        '1945-07-30', NULL,         'Boulogne-Billancourt', 'https://fr.wikipedia.org/wiki/Patrick_Modiano'),
+    (22, 'Nothomb',      'Amélie',       'Belge',            '1966-08-13', NULL,         'Etterbeek',            'https://fr.wikipedia.org/wiki/Amélie_Nothomb'),
+    (23, 'Kerangal',     'Maylis de',    'Française',        '1967-06-21', NULL,         'Toulon',               'https://fr.wikipedia.org/wiki/Maylis_de_Kerangal'),
+    (24, 'Kubrick',      'Stanley',      'Américaine',       '1928-07-26', '1999-03-07', 'New York',             'https://fr.wikipedia.org/wiki/Stanley_Kubrick'),
+    (25, 'Kurosawa',     'Akira',        'Japonaise',        '1910-03-23', '1998-09-06', 'Tokyo',                'https://fr.wikipedia.org/wiki/Akira_Kurosawa'),
+    (26, 'Godard',       'Jean-Luc',     'Franco-Suisse',    '1930-12-03', '2022-09-13', 'Paris',                'https://fr.wikipedia.org/wiki/Jean-Luc_Godard'),
+    (27, 'Truffaut',     'François',     'Française',        '1932-02-06', '1984-10-21', 'Paris',                'https://fr.wikipedia.org/wiki/François_Truffaut'),
+    (28, 'Davis',        'Miles',        'Américaine',       '1926-05-26', '1991-09-28', 'Alton',                'https://fr.wikipedia.org/wiki/Miles_Davis'),
+    (29, 'Coltrane',     'John',         'Américaine',       '1926-09-23', '1967-07-17', 'Hamlet',               'https://fr.wikipedia.org/wiki/John_Coltrane'),
+    (30, 'Glass',        'Philip',       'Américaine',       '1937-01-31', NULL,         'Baltimore',            'https://fr.wikipedia.org/wiki/Philip_Glass'),
+    (31, 'Goscinny',     'René',         'Française',        '1926-08-14', '1977-11-05', 'Paris',                'https://fr.wikipedia.org/wiki/René_Goscinny'),
+    (32, 'Uderzo',       'Albert',       'Française',        '1927-04-25', '2020-03-24', 'Fismes',               'https://fr.wikipedia.org/wiki/Albert_Uderzo'),
+    (33, 'Hergé',        'Georges Remi', 'Belge',            '1907-05-22', '1983-03-03', 'Etterbeek',            'https://fr.wikipedia.org/wiki/Hergé'),
+    (34, 'Moebius',      'Jean Giraud',  'Française',        '1938-05-08', '2012-03-10', 'Nogent-sur-Marne',     'https://fr.wikipedia.org/wiki/Jean_Giraud'),
+    (35, 'Hessel',       'Stéphane',     'Franco-Allemande', '1917-10-20', '2013-02-27', 'Berlin',               'https://fr.wikipedia.org/wiki/Stéphane_Hessel'),
+    (36, 'Todd',         'Emmanuel',     'Française',        '1951-05-16', NULL,         'Saint-Germain',        'https://fr.wikipedia.org/wiki/Emmanuel_Todd'),
+    (37, 'Hemingway',    'Ernest',       'Américaine',       '1899-07-21', '1961-07-02', 'Oak Park',             'https://fr.wikipedia.org/wiki/Ernest_Hemingway'),
+    (38, 'Nabokov',      'Vladimir',     'Russo-Américaine', '1899-04-22', '1977-07-02', 'Saint-Pétersbourg',    'https://fr.wikipedia.org/wiki/Vladimir_Nabokov'),
+    (39, 'Saramago',     'José',         'Portugaise',       '1922-11-16', '2010-06-18', 'Azinhaga',             'https://fr.wikipedia.org/wiki/José_Saramago'),
+    (40, 'Sciamma',      'Céline',       'Française',        '1978-11-12', NULL,         'Pontoise',             'https://fr.wikipedia.org/wiki/Céline_Sciamma'),
+    (41, 'Tarantino',    'Quentin',      'Américaine',       '1963-03-27', NULL,         'Knoxville',            'https://fr.wikipedia.org/wiki/Quentin_Tarantino'),
+    (42, 'Williams',     'John',         'Américaine',       '1932-02-08', NULL,         'Floral Park',          'https://fr.wikipedia.org/wiki/John_Williams_(compositeur)'),
+    (43, 'Moore',        'Alan',         'Britannique',      '1953-11-18', NULL,         'Northampton',          'https://fr.wikipedia.org/wiki/Alan_Moore'),
+    (44, 'Bauza',        'Antoine',      'Française',        '1978-05-31', NULL,         'Paris',                'https://fr.wikipedia.org/wiki/Antoine_Bauza'),
+    (45, 'Reeves',       'Matt',         'Américaine',       '1966-04-27', NULL,         'Rockville Centre',     'https://fr.wikipedia.org/wiki/Matt_Reeves'),
+    (46, 'Teuber',       'Klaus',        'Allemande',        '1952-06-25', '2023-04-01', 'Rai-Breitenbach',      'https://fr.wikipedia.org/wiki/Klaus_Teuber'),
+    (47, 'Miyamoto',       'Shigeru',         'Japonaise',       '1958-11-16', NULL,         'Sonobe',     'https://fr.wikipedia.org/wiki/Shigeru_Miyamoto'),
+    (48, 'Gilligan',       'Vince',        'Américaine',        '1967-02-10', NULL, 'Richmond',      'https://fr.wikipedia.org/wiki/Vince_Gilligan'),
+    (49, 'Teuber',       'Klaus ',        'Allemande',        '1952-06-25', '2023-04-01', 'Breuberg',      'https://en.wikipedia.org/wiki/Klaus_Teuber');
 
 
 -- ------------------------------------------------------------
@@ -136,6 +173,7 @@ INSERT INTO document (
     id, titre, description, lien_gif, code_emplacement, code_isbn, 
     code_emprunt, specificite, date_acquisition, date_publication, 
     created_at, updated_at, auteur_id, format_id, editeur_id, type_id, bibliotheque_id) VALUES
+
 -- === LIVRES (type_id = 1) ===
     (1, '1984', '328 pages. Roman dystopique dans lequel Big Brother surveille tout.', 'https://cdn.elidia.io/.uploads/.img/FIC171037.jpg', 'A1-001', '9782070368228', 'NON', 'MAUVAIS_ETAT', '2018-03-10', '1949-06-08', '2018-03-10 09:00:00', '2024-01-15 10:00:00', 1, 1, 3, 1, 1),
     (2, 'La Ferme des animaux', '144 pages. Fable politique allégorique sur le totalitarisme.', 'https://cdn.cultura.com/cdn-cgi/image/width=830/media/pim/TITELIVE/10_9782081521490_1_75.jpg', 'A1-002', '9782070360024', 'OUI', '', '2018-03-10', '1945-08-17', '2018-03-10 09:05:00', '2024-01-15 10:00:00', 1, 1, 3, 1, 1),
@@ -150,10 +188,10 @@ INSERT INTO document (
     (13, 'Inception', '148 min. Un voleur s''infiltre dans les rêves pour y implanter une idée.', 'https://m.media-amazon.com/images/M/MV5BZjhkNjM0ZTMtNGM5MC00ZTQ3LTk3YmYtZTkzYzdiNWE0ZTA2XkEyXkFqcGc@._V1_.jpg', 'C3-001', NULL, 'OUI', '', '2020-11-20', '2010-07-16', '2020-11-20 14:00:00', '2024-03-01 08:00:00', 5, 4, 5, 2, 1),
     (14, 'E.T. l''extra-terrestre', '115 min. Un enfant se lie d''amitié avec un extraterrestre perdu sur Terre.', 'https://cinema-tarasconsurariege.fr/wp-content/uploads/2025/08/extra-terrestre.jpg', 'C3-002', NULL, 'OUI', '', '2018-08-01', '1982-06-11', '2018-08-01 09:00:00', '2024-03-01 08:00:00', 6, 4, 6, 2, 1),
     (15, 'Le Voyage de Chihiro', '125 min. Une fillette doit travailler dans des thermes pour esprits pour sauver ses parents.', 'https://m.media-amazon.com/images/M/MV5BOWNhM2Y3NmUtN2Y4Ni00YjA1LThjNGItYTlkNTU2MWFkMzNkXkEyXkFqcGc@._V1_.jpg', 'C3-003', NULL, 'OUI', '', '2021-04-05', '2001-07-20', '2021-04-05 10:00:00', '2024-03-20 09:00:00', 9, 4, 6, 2, 1),
-    (16, 'Dune (2021)', '155 min. Adaptation du roman de Frank Herbert par Denis Villeneuve.', 'https://www.allocine.fr/film/fichefilm_gen_cfilm=133392.html', 'C3-004', NULL, 'OUI', '', '2022-01-15', '2021-09-15', '2022-01-15 10:00:00', '2024-03-20 09:00:00', 12, 4, 5, 2, 1),
+    (16, 'Dune (2021)', '155 min. Adaptation du roman de Frank Herbert par Denis Villeneuve.', 'https://m.media-amazon.com/images/I/71XlZvKMwoL.jpg', 'C3-004', NULL, 'OUI', '', '2022-01-15', '2021-09-15', '2022-01-15 10:00:00', '2024-03-20 09:00:00', 12, 4, 5, 2, 1),
     (17, 'Interstellar — Blu-ray', '169 min. Édition Blu-ray collector du film de Christopher Nolan.', 'https://m.media-amazon.com/images/I/91vIHsL-zjL._AC_UF1000,1000_QL80_.jpg', 'D4-001', NULL, 'NON', 'VALEUR', '2022-05-10', '2014-11-07', '2022-05-10 11:00:00', '2024-04-10 09:00:00', 5, 5, 5, 2, 2),
     (18, 'Le Château Ambulant — Blu-ray', '119 min. Adaptation du roman de Diana Wynne Jones par Miyazaki.', 'https://images.justwatch.com/poster/185096351/s718/le-chateau-ambulant.jpg', 'D4-002', NULL, 'OUI', '', '2023-02-20', '2004-11-20', '2023-02-20 10:00:00', '2024-04-10 09:00:00', 9, 5, 6, 2, 2),
-    (19, 'Shining — Blu-ray', '146 min. Adaptation du roman de Stephen King par Stanley Kubrick.', 'https://m.media-amazon.com/images/I/A1cWZr+V3cL._AC_UF894,1000_QL80_.jpg', 'D4-003', NULL, 'OUI', '', '2020-10-31', '1980-05-23', '2020-10-31 13:00:00', '2024-04-10 09:00:00', 4, 5, 5, 2, 2),
+    (19, 'Shining — Blu-ray', '146 min. Adaptation du roman de Stephen King par Stanley Kubrick.', 'https://m.media-amazon.com/images/I/A1cWZr+V3cL._AC_UF894,1000_QL80_.jpg', 'D4-003', NULL, 'OUI', '', '2020-10-31', '1980-05-23', '2020-10-31 13:00:00', '2024-04-10 09:00:00', 24, 5, 5, 2, 2),
     (26, 'Tenet', '150 min. Un agent secret utilise l''inversion du temps pour empêcher la Troisième Guerre mondiale.', 'https://m.media-amazon.com/images/M/MV5BYjI0NDQzYmEtNzMwZC00ODA3LTgzZDYtZTk5ODZjY2Y2OTkzXkEyXkFqcGc@._V1_.jpg', 'C3-005', NULL, 'OUI', '', '2021-01-15', '2020-08-26', '2021-01-15 10:00:00', '2024-01-15 10:00:00', 5, 4, 5, 2, 2),
     (27, 'The Batman', '176 min. Le Chevalier Noir enquête sur une série de crimes commis par le Sphinx à Gotham City.', 'https://www.lescomics.fr/wp-content/uploads/2022/03/the-batman-2022-i120267.jpg', 'C3-006', NULL, 'OUI', '', '2022-07-20', '2022-03-02', '2022-07-20 11:00:00', '2024-01-15 10:00:00', 5, 4, 5, 2, 2),
 
@@ -166,21 +204,20 @@ INSERT INTO document (
 -- === ALBUMS / MUSIQUE (type_id = 5) ===
     (9, 'Songs of Leonard Cohen', '41 min (10 pistes). Premier album studio de Leonard Cohen, folk introspectif.', 'https://m.media-amazon.com/images/I/61vq7Y0LClL._UF894,1000_QL80_.jpg', 'B2-001', NULL, 'OUI', '', '2019-02-14', '1967-12-27', '2019-02-14 15:00:00', '2024-01-20 10:00:00', 8, 3, 4, 5, 1),
     (10, 'I''m Your Man', '40 min (8 pistes). Album synthpop et cabaret, synthèse de l''œuvre de Cohen.', 'https://m.media-amazon.com/images/I/611K+pBfrkL.jpg', 'B2-002', NULL, 'OUI', '', '2019-02-14', '1988-02-01', '2019-02-14 15:10:00', '2024-01-20 10:00:00', 8, 3, 4, 5, 1),
-    (11, 'Interstellar — Bande originale', '71 min (16 pistes). Composition orchestrale et orgue pour le film de Christopher Nolan.', 'https://cdn-images.dzcdn.net/images/cover/4193cfd449576b01d33adeb54c2589ce/0x1900-000000-80-0-0.jpg', 'B2-003', NULL, 'OUI', '', '2021-03-20', '2014-11-17', '2021-03-20 09:30:00', '2024-02-15 08:00:00', 7, 3, 4, 5, 2),
+    (11, 'Interstellar — Bande originale', '71 min (16 pistes). Composition orchestrale et orgue pour le film de Christopher Nolan.', 'https://cdn-images.dzcdn.net/images/cover/4193cfd449576b01d33adeb54c2589ce/0x1900-000000-80-0-0.jpg', 'B2-003', NULL, 'OUI', '', '2021-03-20', '2014-11-17', '2021-03-20 09:30:00', '2024-02-15 08:00:00', 31, 3, 4, 5, 2),
     (12, 'Dune — Bande originale (2021)', '74 min (9 pistes). Bande originale de Denis Villeneuve, composition de Hans Zimmer.', 'https://m.media-amazon.com/images/I/81XLwfmntwL._UF894,1000_QL80_.jpg', 'B2-004', NULL, 'OUI', '', '2022-01-10', '2021-09-03', '2022-01-10 10:00:00', '2024-02-15 08:00:00', 7, 3, 4, 5, 2),
 
 -- === BANDES DESSINÉES (type_id = 6) ===
     (22, 'Astérix le Gaulois', '48 pages. Première aventure mythique d''Astérix et Obélix contre les Romains.', 'https://cdn.cultura.com/cdn-cgi/image/width=830/media/pim/TITELIVE/17_9782012101333_1_75.jpg', 'G7-001', '9782012101333', 'OUI', '', '2022-03-15', '1961-10-01', '2022-03-15 10:00:00', '2024-01-15 10:00:00', 4, 2, 3, 6, 1),
 
 -- === JEUX VIDÉO (type_id = 7) ===
-    (23, 'The Legend of Zelda: Breath of the Wild', 'Jeu Console (Switch/WiiU). Jeu vidéo d''action-aventure en monde ouvert.', 'https://static.posters.cz/image/1300/toiles-the-legend-of-zelda-breath-of-the-wild-sunset-i111061.jpg', 'H8-001', NULL, 'OUI', '', '2021-06-20', '2017-03-03', '2021-06-20 14:00:00', '2024-02-01 09:00:00', 5, 3, 5, 7, 2),
+    (23, 'The Legend of Zelda: Breath of the Wild', 'Jeu Console (Switch/WiiU). Jeu vidéo d''action-aventure en monde ouvert.', 'https://static.posters.cz/image/1300/toiles-the-legend-of-zelda-breath-of-the-wild-sunset-i111061.jpg', 'H8-001', NULL, 'OUI', '', '2021-06-20', '2017-03-03', '2021-06-20 14:00:00', '2024-02-01 09:00:00', 47, 3, 5, 7, 2),
 
 -- === SÉRIES TV (type_id = 8) ===
-    (24, 'Breaking Bad - Intégrale Saison 1', '340 min (7 épisodes). Série télévisée dramatique suivant l''évolution de Walter White.', 'https://static.fnac-static.com/multimedia/FR/images_produits/FR/Fnac.com/ZoomPE/6/3/7/3333297600736/tsp20130903125224/Coffret-integral-de-la-Saison-1.jpg', 'I9-001', NULL, 'OUI', '', '2020-05-12', '2008-01-20', '2020-05-12 11:00:00', '2024-03-01 10:00:00', 6, 4, 6, 8, 1),
+    (24, 'Breaking Bad - Intégrale Saison 1', '340 min (7 épisodes). Série télévisée dramatique suivant l''évolution de Walter White.', 'https://static.fnac-static.com/multimedia/FR/images_produits/FR/Fnac.com/ZoomPE/6/3/7/3333297600736/tsp20130903125224/Coffret-integral-de-la-Saison-1.jpg', 'I9-001', NULL, 'OUI', '', '2020-05-12', '2008-01-20', '2020-05-12 11:00:00', '2024-03-01 10:00:00', 48, 4, 6, 8, 1),
 
 -- === JEUX DE SOCIÉTÉ (type_id = 9) ===
-    (25, 'Catan', 'Partie de 60-90 min (3-4 joueurs). Jeu de plateau, de stratégie et de négociation de ressources.', 'https://lagranderecre-lagranderecre-fr-storage.omn.proximis.com/Imagestorage/imagesSynchro/0/0/9d5db7bbfc6e425ec1ffbd294c22ea8fd60566c9_IMG-PRODUCT-206065-6.jpeg', 'J10-001', NULL, 'OUI', '', '2023-11-18', '1995-01-01', '2023-11-18 15:00:00', '2023-11-18 15:00:00', 11, 2, 2, 9, 2);
-
+    (25, 'Catan', 'Partie de 60-90 min (3-4 joueurs). Jeu de plateau, de stratégie et de négociation de ressources.', 'https://lagranderecre-lagranderecre-fr-storage.omn.proximis.com/Imagestorage/imagesSynchro/0/0/9d5db7bbfc6e425ec1ffbd294c22ea8fd60566c9_IMG-PRODUCT-206065-6.jpeg', 'J10-001', NULL, 'OUI', '', '2023-11-18', '1995-01-01', '2023-11-18 15:00:00', '2023-11-18 15:00:00', 49, 2, 2, 9, 2);
 -- ------------------------------------------------------------
 --  ASSOCIATIONS DOCUMENT <-> GENRE
 -- ------------------------------------------------------------
